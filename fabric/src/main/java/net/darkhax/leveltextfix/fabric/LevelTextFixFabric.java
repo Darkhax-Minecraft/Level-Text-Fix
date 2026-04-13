38daa226b7cf46f6172327641f246599536446fa
Tyler Hancock <darklime@live.ca>
2026-04-13 02:38:06 -0600

    Add missing files for 26.1.2

---
+++ fabric/src/main/java/net/darkhax/leveltextfix/fabric/LevelTextFixFabric.java
@@ -0,0 +1,9 @@
+package net.darkhax.leveltextfix.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LevelTextFixFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
